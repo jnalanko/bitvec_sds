84903571e38811b72ec84758c7e22475fe9abc03
Jarno N. Alanko <alanko.jarno@gmail.com>
2026-02-08 10:10:00 +0200

    Benchmarking

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,7 @@ use rand_xoshiro::{Xoshiro256PlusPlus, rand_core::{RngCore, SeedableRng}};
 
 use crate::{rank_support_v::RankSupportV, traits::{Pat1, Sel1}};
 fn main() {
-    benchmark();
-}
 
-fn benchmark() {
-    // Generate a bitvector of with 8 billion bits randomly set
     let mut rng = Xoshiro256PlusPlus::seed_from_u64(123);
     let n = 8_000_000_000usize;
     let mut bv: BitVec<u64, Lsb0> = BitVec::with_capacity(n);
@@ -21,10 +17,17 @@ fn benchmark() {
     for _ in 0..n {
         bv.push(rng.next_u64() % 2 == 0);
     }
-    let n_ones = bv.count_ones();
+
+    let bv = Arc::new(bv);
+    benchmark_rank(bv.clone());
+    benchmark_select(bv.clone());
+}
+
+fn benchmark_rank(bv: Arc<BitVec<u64, Lsb0>>) {
+    let mut rng = Xoshiro256PlusPlus::seed_from_u64(131313);
+    let n = bv.len();
 
     print!("Bitvector generated. Building RankSupportV... ");
-    let bv = Arc::new(bv);
     let rank_support = RankSupportV::<Pat1>::new(bv.clone());
 
     // Draw 1 million random query positions
@@ -52,8 +55,14 @@ fn benchmark() {
     let elapsed = start.elapsed();
     let avg_time_per_query = elapsed.as_secs_f64() / n_queries as f64;
     println!("Average time per rank query: {:.2} ns", avg_time_per_query * 1e9);
+}
+
+fn benchmark_select(bv: Arc<BitVec<u64, Lsb0>>) {
+    let mut rng = Xoshiro256PlusPlus::seed_from_u64(131313);
+    let n_ones = bv.count_ones();
 
     // Benchmark select queries
+    let n_queries = 1_000_000;
     let select_support = crate::select_support_mcl::SelectSupportMcl::<Sel1>::new(bv.clone());
     let max_select_query = n_ones;
     let mut select_query_positions: Vec<usize> = Vec::with_capacity(n_queries);
